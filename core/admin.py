95057e19299dde6c249768a56139cbe605a4a265
Nycollas Henich <nhhenich@gmail.com>
2024-06-17 15:21:19 -0300

    models add

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Cliente
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+    # mostar no admin
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cliente, ClienteAdmin)
--- setup/urls.py
+++ setup/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('painel/', admin.site.urls),
+    # painel = antigo admin/
     path('', include('core.urls')),
 ]
