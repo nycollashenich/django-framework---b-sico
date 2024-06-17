c81266eda6d1d70e08cd76eda1dc8446f4287385
Nycollas Henich <nhhenich@gmail.com>
2024-06-16 23:51:45 -0300

    introduzindo Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# python manage.py makemigrations
+# python manage.py migrate
+
+class Product(models.Model):
+    nome = models.CharField('Nome', max_length= 100)
+    preco = models.DecimalField('Preço', decimal_places= 2, max_digits=8)
+    estoque = models.IntegerField("Quantidade em Estoque")
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length= 100)
+    sobrenome = models.CharField('Sobrenome', max_length= 100)
+    email = models.EmailField(max_length=254)
