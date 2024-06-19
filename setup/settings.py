c31f2c6ba9475b3e255555206eff9d3db3a75bfb
Nycollas Henich <nhhenich@gmail.com>
2024-06-19 12:04:14 -0300

    add logout do admin

--- setup/settings.py
+++ setup/settings.py
@@ -125,3 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# para redirecinar após o logout do admin, clicou voltou
+LOGOUT_REDIRECT_URL = 'index'
